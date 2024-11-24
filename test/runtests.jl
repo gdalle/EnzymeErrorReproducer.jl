f9e2fff29a98b97b1f28df6b9cba8571357d5e9b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 23:00:47 +0100

    Split further

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,22 @@ Enzyme.Compiler.DumpPostOpt[] = true
 using EnzymeErrorReproducer
 using Test
 
+FUNCTION = get(ENV, "JULIA_HVP_FUNCTION", "bad")
+
 @testset verbose = true "EnzymeErrorReproducer" begin
     x, dx = inputs()
     y, h = outputs()
-    @testset "f_bad" begin
-        @test f_bad(x) == y
-        @test myhvp(f_bad, x, dx) == h
-    end
-    @testset "f_good" begin
-        @test f_good(x) == y
-        @test myhvp(f_good, x, dx) == h
+    if FUNCTION == "bad"
+        @testset "f_bad" begin
+            @test f_bad(x) == y
+            @test myhvp(f_bad, x, dx) == h
+        end
+    elseif FUNCTION == "good"
+        @testset "f_good" begin
+            @test f_good(x) == y
+            @test myhvp(f_good, x, dx) == h
+        end
+    else
+        error("Unknown function $FUNCTION")
     end
 end

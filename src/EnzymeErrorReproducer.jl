7285c16d298d7d86b54c91c1cae3802d58f0473e
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 08:39:11 +0100

    With and without check bounds

--- src/EnzymeErrorReproducer.jl
+++ src/EnzymeErrorReproducer.jl
@@ -2,6 +2,11 @@ module EnzymeErrorReproducer
 
 using Enzyme
 
+Enzyme.API.printall!(true)
+Enzyme.Compiler.DumpPreEnzyme[] = true
+Enzyme.Compiler.DumpPostWrap[] = true
+Enzyme.Compiler.DumpPostOpt[] = true
+
 function mygradient(f, x)
     g = zero(x)
     autodiff(Reverse, f, Active, Duplicated(x, g))
@@ -12,8 +17,8 @@ myhvp(f, x, dx) = return autodiff(Forward, mygradient, Const(f), Duplicated(x, d
 
 @inline function arraycube_bad(x::AbstractArray{N}) where {N}
     y = similar(x)
-    for i in 1:4
-        for j in 1:3
+    @inbounds for i in 1:4
+        @inbounds for j in 1:3
             y[i, j] = x[i, j]^3
         end
     end
@@ -22,7 +27,7 @@ end
 
 @inline function arraycube_good(x)
     y = similar(x)
-    for k in 1:12
+    @inbounds for k in 1:12
         y[k] = x[k]^3
     end
     return y
@@ -31,7 +36,10 @@ end
 f_bad(x) = sum(arraycube_bad(x))
 f_good(x) = sum(arraycube_good(x))
 
+inputs() = Matrix{Float64}(reshape(1:12, 4, 3)), ones(4, 3)
+
 export f_bad, f_good
 export myhvp
+export inputs
 
 end # module EnzymeErrorReproducer
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EnzymeErrorReproducer
 using Test
 
 @testset "EnzymeErrorReproducer" begin
-    xm, dxm = Matrix{Float64}(reshape(1:12, 4, 3)), ones(4, 3)
+    xm, dxm = inputs()
     @test f_bad(xm) == f_good(xm)
     @test myhvp(f_bad, xm, dxm) == myhvp(f_good, xm, dxm)
 end
